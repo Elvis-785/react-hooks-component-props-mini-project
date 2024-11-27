82b7fcca3e7ef9a3b36d6989155fcc80f69aab54
Elvis-785 <kiruielvis3358@gmail.com>
2024-11-28 00:50:14 +0300

    components and props mini project on react

---
+++ src/components/Article.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const minutesToRead = minutes < 30 ? 
+    "☕️".repeat(Math.ceil(minutes / 5)) + ` ${minutes} min read` : 
+    "🍱".repeat(Math.ceil(minutes / 10)) + ` ${minutes} min read`;
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date} • {minutesToRead}</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
